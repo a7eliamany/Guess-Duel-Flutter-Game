cfb6778cf3682d36550453d23a25b26ae90293ef
Ahmed Eliamany <a7medeliamany234@gmail.com>
2026-08-25 13:06:46 +0300

    edit attempt history apperence in online game (like watsapp)

--- lib/screens/game screen/game_screen.dart
+++ lib/screens/game screen/game_screen.dart
@@ -21,63 +21,62 @@ class GameScreen extends HookWidget {
     useEffect(() {
       context.read<AttemptsCubit>().getAttempts(roomID);
       context.read<PlayerTurnCubit>().checkPlayerTurn(roomID);
+
       return null;
     }, [roomID]);
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 20),
+        child: Column(
           children: [
             const SizedBox(height: 24),
             // Status Indicator
-            Row(
-              children: [
-                const Spacer(),
-                GameHeader(roomID: roomID),
-                const Spacer(),
-              ],
-            ),
+            GameHeader(roomID: roomID),
 
-            const Center(
-              child: Column(
+            Expanded(
+              child: ListView(
                 children: [
-                  SizedBox(height: 8),
-                  Text(
+                  const SizedBox(height: 8),
+                  const Text(
                     'Enter your 4-digit code',
+                    textAlign: TextAlign.center,
                     style: TextStyle(
                       color: Color(0xFFadaaaa),
                       fontSize: 16,
                       fontWeight: FontWeight.w500,
                     ),
                   ),
+
+                  const SizedBox(height: 16),
+
+                  // Input Section
+                  const CodeInput(),
+
+                  const SizedBox(height: 32),
+                  // Keypad
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 50),
+                    child: Numpad(),
+                  ),
+                  const SizedBox(height: 16),
+
+                  BlocBuilder<NumpadCubit, NumpadState>(
+                    builder: (context, state) {
+                      return SubmitButton(code: state.code, roomID: roomID);
+                    },
+                  ),
+
+                  const SizedBox(height: 32),
+
+                  // History Section
+                  const HistoryTitle(),
+                  Divider(
+                    color: const Color(0xFF767575).withValues(alpha: 0.1),
+                  ),
+                  const AttemptsHistory(),
                 ],
               ),
             ),
-            const SizedBox(height: 32),
-
-            // Input Section
-            const CodeInput(),
-
-            const SizedBox(height: 32),
-            // Keypad
-            const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 50),
-              child: Numpad(),
-            ),
-            const SizedBox(height: 16),
-
-            BlocBuilder<NumpadCubit, NumpadState>(
-              builder: (context, state) {
-                return SubmitButton(code: state.code, roomID: roomID);
-              },
-            ),
-
-            const SizedBox(height: 32),
-
-            // History Section
-            const HistoryTitle(),
-            Divider(color: const Color(0xFF767575).withValues(alpha: 0.1)),
-            const AttemptsHistory(),
           ],
         ),
       ),
--- lib/screens/game screen/widgets/attempts_history.dart
+++ lib/screens/game screen/widgets/attempts_history.dart
@@ -28,165 +28,195 @@ class AttemptsHistory extends StatelessWidget {
                   AttemptModel attempt = state.attempts[index];
                   bool isYourAttempt = attempt.userId == userID;
 
-                  return Container(
-                    margin: const EdgeInsets.only(bottom: 8),
-                    padding: const EdgeInsets.all(16),
-                    decoration: BoxDecoration(
-                      color: isYourAttempt
-                          ? const Color(0xFF1a1a1a)
-                          : const Color(0xFF131313),
-                      borderRadius: BorderRadius.circular(12),
-                      border: Border.all(
-                        color: const Color(
-                          0xFF767575,
-                        ).withValues(alpha: isYourAttempt ? 0.1 : 0.05),
+                  return Align(
+                    alignment: isYourAttempt
+                        ? Alignment.centerRight
+                        : Alignment.centerLeft,
+                    child: Container(
+                      margin: const EdgeInsets.only(bottom: 8),
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: isYourAttempt
+                            ? const Color(0xFF1a1a1a)
+                            : const Color(0xFF131313),
+                        borderRadius: BorderRadius.circular(12),
+                        border: Border.all(
+                          color: const Color(
+                            0xFF767575,
+                          ).withValues(alpha: isYourAttempt ? 0.1 : 0.05),
+                        ),
                       ),
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          attempt.attempt,
-                          style: TextStyle(
-                            fontFamily: 'Space Grotesk',
-                            fontSize: 20,
-                            letterSpacing: 2,
-                            color: isYourAttempt
-                                ? Colors.white
-                                : Colors.white.withValues(alpha: 0.6),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+
+                        // يعكس ترتيب العناصر
+                        textDirection: isYourAttempt
+                            ? TextDirection.rtl
+                            : TextDirection.ltr,
+
+                        children: [
+                          // NUMBER
+                          Text(
+                            attempt.attempt,
+                            style: TextStyle(
+                              fontFamily: 'Space Grotesk',
+                              fontSize: 20,
+                              letterSpacing: 2,
+                              color: isYourAttempt
+                                  ? Colors.white
+                                  : Colors.white.withValues(alpha: 0.6),
+                            ),
                           ),
-                        ),
-                        Row(
-                          children: [
-                            if (attempt.correctNumbers > 0)
-                              Row(
-                                children: [
-                                  Text(
-                                    '${attempt.correctNumbers} Correct',
-                                    style: TextStyle(
-                                      color: isYourAttempt
-                                          ? const Color(0xFFa9ffac)
-                                          : const Color(
-                                              0xFFa9ffac,
-                                            ).withValues(alpha: 0.6),
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.bold,
-                                      letterSpacing: 1,
+
+                          const SizedBox(width: 20),
+
+                          // RESULT
+                          Column(
+                            mainAxisSize: MainAxisSize.min,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+
+                            children: [
+                              if (attempt.correctNumbers > 0)
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                      '${attempt.correctNumbers} Correct',
+                                      style: TextStyle(
+                                        color: isYourAttempt
+                                            ? const Color(0xFFa9ffac)
+                                            : const Color(
+                                                0xFFa9ffac,
+                                              ).withValues(alpha: 0.6),
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold,
+                                        letterSpacing: 1,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(width: 4),
-                                  Row(
-                                    children: List.generate(
-                                      attempt.correctNumbers,
-                                      (i) => Container(
-                                        width: 6,
-                                        height: 6,
-                                        margin: const EdgeInsets.only(right: 2),
-                                        decoration: BoxDecoration(
-                                          color: isYourAttempt
-                                              ? const Color(0xFFa9ffac)
-                                              : const Color(
-                                                  0xFFa9ffac,
-                                                ).withValues(alpha: 0.4),
-                                          shape: BoxShape.circle,
+
+                                    const SizedBox(width: 4),
+
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: List.generate(
+                                        attempt.correctNumbers,
+                                        (i) => Container(
+                                          width: 6,
+                                          height: 6,
+                                          margin: const EdgeInsets.only(
+                                            right: 2,
+                                          ),
+                                          decoration: BoxDecoration(
+                                            color: isYourAttempt
+                                                ? const Color(0xFFa9ffac)
+                                                : const Color(
+                                                    0xFFa9ffac,
+                                                  ).withValues(alpha: 0.4),
+                                            shape: BoxShape.circle,
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            if (attempt.correctPlaces > 0)
-                              Row(
-                                children: [
-                                  Container(
-                                    height: 12,
-                                    width: 1,
-                                    color: const Color(
-                                      0xFF767575,
-                                    ).withValues(alpha: 0.2),
-                                    margin: const EdgeInsets.symmetric(
-                                      horizontal: 8,
+                                  ],
+                                ),
+
+                              if (attempt.correctPlaces > 0)
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Text(
+                                      '${attempt.correctPlaces} positioned',
+                                      style: TextStyle(
+                                        color: isYourAttempt
+                                            ? const Color(0xFFd674ff)
+                                            : const Color(
+                                                0xFFd674ff,
+                                              ).withValues(alpha: 0.6),
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold,
+                                        letterSpacing: 1,
+                                      ),
                                     ),
-                                  ),
-                                  Text(
-                                    '${attempt.correctPlaces} postioned',
-                                    style: TextStyle(
-                                      color: isYourAttempt
-                                          ? const Color(0xFFd674ff)
-                                          : const Color(
-                                              0xFFd674ff,
-                                            ).withValues(alpha: 0.6),
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.bold,
-                                      letterSpacing: 1,
-                                    ),
-                                  ),
-                                  const SizedBox(width: 4),
-                                  Row(
-                                    children: List.generate(
-                                      attempt.correctPlaces,
-                                      (i) => Container(
-                                        width: 6,
-                                        height: 6,
-                                        margin: const EdgeInsets.only(right: 2),
-                                        decoration: BoxDecoration(
-                                          color: isYourAttempt
-                                              ? const Color(0xFFd674ff)
-                                              : const Color(
-                                                  0xFFd674ff,
-                                                ).withValues(alpha: 0.4),
-                                          shape: BoxShape.circle,
+
+                                    const SizedBox(width: 4),
+
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: List.generate(
+                                        attempt.correctPlaces,
+                                        (i) => Container(
+                                          width: 6,
+                                          height: 6,
+                                          margin: const EdgeInsets.only(
+                                            right: 2,
+                                          ),
+                                          decoration: BoxDecoration(
+                                            color: isYourAttempt
+                                                ? const Color(0xFFd674ff)
+                                                : const Color(
+                                                    0xFFd674ff,
+                                                  ).withValues(alpha: 0.4),
+                                            shape: BoxShape.circle,
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                            if (attempt.correctNumbers < 1 &&
-                                attempt.correctPlaces < 1)
-                              Row(
-                                children: [
-                                  Container(
-                                    height: 12,
-                                    width: 1,
-                                    color: const Color(
-                                      0xFF767575,
-                                    ).withValues(alpha: 0.2),
-                                    margin: const EdgeInsets.symmetric(
-                                      horizontal: 8,
+                                  ],
+                                ),
+
+                              if (attempt.correctNumbers < 1 &&
+                                  attempt.correctPlaces < 1)
+                                Row(
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    Container(
+                                      height: 12,
+                                      width: 1,
+                                      color: const Color(
+                                        0xFF767575,
+                                      ).withValues(alpha: 0.2),
+                                      margin: const EdgeInsets.symmetric(
+                                        horizontal: 8,
+                                      ),
                                     ),
-                                  ),
-                                  Text(
-                                    '4 Wrong',
-                                    style: TextStyle(
-                                      color: isYourAttempt
-                                          ? Colors.red
-                                          : Colors.red.withValues(alpha: 0.6),
-                                      fontSize: 10,
-                                      fontWeight: FontWeight.bold,
-                                      letterSpacing: 1,
+
+                                    Text(
+                                      '4 Wrong',
+                                      style: TextStyle(
+                                        color: isYourAttempt
+                                            ? Colors.red
+                                            : Colors.red.withValues(alpha: 0.6),
+                                        fontSize: 12,
+                                        fontWeight: FontWeight.bold,
+                                        letterSpacing: 1,
+                                      ),
                                     ),
-                                  ),
-                                  const SizedBox(width: 4),
-                                  Row(
-                                    children: List.generate(
-                                      4,
-                                      (i) => Container(
-                                        width: 6,
-                                        height: 6,
-                                        margin: const EdgeInsets.only(right: 2),
-                                        decoration: const BoxDecoration(
-                                          color: Colors.red,
-                                          shape: BoxShape.circle,
+
+                                    const SizedBox(width: 4),
+
+                                    Row(
+                                      mainAxisSize: MainAxisSize.min,
+                                      children: List.generate(
+                                        4,
+                                        (i) => Container(
+                                          width: 6,
+                                          height: 6,
+                                          margin: const EdgeInsets.only(
+                                            right: 2,
+                                          ),
+                                          decoration: const BoxDecoration(
+                                            color: Colors.red,
+                                            shape: BoxShape.circle,
+                                          ),
                                         ),
                                       ),
                                     ),
-                                  ),
-                                ],
-                              ),
-                          ],
-                        ),
-                      ],
+                                  ],
+                                ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
                   );
                 },
